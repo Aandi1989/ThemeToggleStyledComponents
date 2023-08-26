cf92ca0a63384bd3fa4f7e7ef15c3625542cb33c
Aandi1989 <navaliny1989@gmail.com>
2023-08-26 13:02:19 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from "styled-components";
 import MoonIcon from "./components/icons/MoonIcon";
 import SunIcon from "./components/icons/SunIcon";
 import Switch from "./components/Switch";
 
-const style = {
-  minHeight: "100vh",
-  textAlign: "center",
-  paddingTop: "10rem",
-  backgroundColor: "#f5f5f5",
+const StyledApp = styled.div`
+  min-height: 100vh;
+  text-align: center;
+  padding-top: 10rem;
+  background-color: ${(props) => props.theme.body};
+  transition: all 0.25s ease;
+`;
+
+const Name = styled.h1`
+  margin: 1rem;
+  color: ${(props) => props.theme.title};
+`;
+
+const Info = styled.p`
+  margin: 1rem;
+  color: ${(props) => props.theme.subtitle};
+`;
+
+const darkTheme = {
+  body: "#1c1c1c",
+  title: "#fff",
+  subtitle: "#b6b6b6",
+  icon: "#b6b6b6",
+};
+const lightTheme = {
+  body: "#fff",
+  title: "#1c1c1c",
+  subtitle: "#333",
+  icon: "#1c1c1c",
 };
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const isDarkTheme = theme === "dark";
+
+  const toggleTheme = () => {
+    setTheme(isDarkTheme ? 'light' : 'dark')
+  }
+
   return (
-    <div style={style}>
-      <SunIcon />
-      <Switch />
-      <MoonIcon />
-      <h1>Jim Naysium</h1>
-      <p>Username: jimnays10</p>
-      <p>Email: jim@gmail.com</p>
-    </div>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <StyledApp>
+        <SunIcon />
+        <Switch toggleTheme={toggleTheme} isDarkTheme={isDarkTheme}/>
+        <MoonIcon />
+        <Name>Aliaksandr Novik</Name>
+        <Info>Username: alex20</Info>
+        <Info>Email: alex@gmail.com</Info>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
